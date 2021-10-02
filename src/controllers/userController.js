bec628dda9a06b719eea5946b9928e6577f62d34
JVBSouza <joseboing@gmail.com>
2021-10-02 15:08:07 -0300

    Update controller and add pagination

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,45 +1,58 @@
 const Users = require("../models/userModel")
 const Router = require('koa-router');
-
-// const router = new Router();
-// const parser = new bodyParser();
+const { INTEGER } = require("sequelize");
+const { request } = require("chai");
 
 const getAllUsers = async (ctx, next) => {
-    const user = await Users.findAll();
-    ctx.body = user;
+  const page = Number(ctx.query.page || 0)
+  const size = Number(ctx.query.size || 10)
+  const user = await Users.findAll({
+    limit: size,
+    offset: page * size
+  });
+  ctx.body = user;
 };
 
 const getUser = async (ctx, next) => {
-    const id = ctx.params.id;
-    const user = await Users.findOne({ where: { id } });
-    ctx.body = user;
+  const id = ctx.params.id;
+  const user = await Users.findOne({ where: { id } });
+  ctx.body = user;
+};
+
+const getUserByName = async (ctx, next) => {
+  const userName = ctx.params.name;
+  console.log(userName)
+  const user = await Users.findOne({ where: { name: userName } });
+
+  ctx.body = user;
 };
 
 const createUser = async (ctx, next) => {
-    const newUser = {
-        name: ctx.request.body.name,
-        email: ctx.request.body.email,
-        age: ctx.request.body.age
-    };
+  const newUser = {
+    name: ctx.request.body.name,
+    email: ctx.request.body.email,
+    age: ctx.request.body.age
+  };
 
-    const user = await Users.create(newUser);
-    ctx.body = user;
+  const user = await Users.create(newUser);
+  ctx.status = 201;
+  ctx.body = user;
 };
 
 const deleteUser = async (ctx, next) => {
-    const id = ctx.params.id;
-    await Users.destroy({ where: { id } });
-    ctx.body = {};
+  const id = ctx.params.id;
+  await Users.destroy({ where: { id } });
+  ctx.body = {};
 };
 
 const updateUser = async (ctx, next) => {
-    const id = ctx.params.id
-    const { ...body } = ctx.request.body;
-    const user = await Users.update(body, { where: { id } });
-    ctx.body = user;
+  const id = ctx.params.id
+  const { ...body } = ctx.request.body;
+  const user = await Users.update(body, { where: { id } });
+  ctx.body = user;
 };
 
-module.exports = { getAllUsers, getUser, createUser, updateUser, deleteUser };
+module.exports = { getAllUsers, getUser, getUserByName, createUser, updateUser, deleteUser };
 
 
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,8 +3,14 @@ const userCRUD = require('../controllers/userController')
 
 const router = new Router();
 
+router.get('/', async (ctx) => {
+    // ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
+    ctx.body = `Seu servidor esta rodando!`; //http://localhost:3000/
+});
+
 router.get("/users", userCRUD.getAllUsers);
 router.get("/users/:id", userCRUD.getUser);
+// router.get("/users/:name", userCRUD.getUserByName);
 router.post("/users", userCRUD.createUser);
 router.put("/users/:id", userCRUD.updateUser);
 router.delete("/users/:id", userCRUD.deleteUser);
