d8c1d6377df8180664051a921a4d9c33476ca55b
JVBSouza <joseboing@gmail.com>
2021-10-02 21:59:03 -0300

    Add user Schema file

--- test/index.js
+++ test/index.js
@@ -6,27 +6,9 @@ const chaiJson = require("chai-json-schema");
 
 chai.use(chaiHttp);
 chai.use(chaiJson);
-
 const expect = chai.expect;
 
-//Define o minimo de campos que o usuário deve ter. Geralmente deve ser colocado em um arquivo separado
-const userSchema = {
-  title: "Schema do Usuario, define como é o usuario, linha 24 do teste",
-  type: "object",
-  required: ["name", "email", "age"],
-  properties: {
-    name: {
-      type: "string",
-    },
-    email: {
-      type: "string",
-    },
-    age: {
-      type: "number",
-      minimum: 18,
-    },
-  },
-};
+const userSchema = require("./schemas/user.json");
 
 //testes da aplicação
 describe("Testes da aplicaçao", () => {
