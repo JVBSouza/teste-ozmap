64a4ceb5789cdc67119515cc36ba1881e04c06cb
JVBSouza <joseboing@gmail.com>
2021-10-03 17:30:24 -0300

    Fix routes and controller and add documentation file

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,4 @@
 const Users = require("../models/userModel");
-const Router = require("koa-router");
-const { INTEGER } = require("sequelize");
-const { request } = require("chai");
 
 const getAllUsers = async (ctx, next) => {
   const page = Number(ctx.query.page || 0);
@@ -31,25 +28,35 @@ const createUser = async (ctx, next) => {
     email: ctx.request.body.email,
     age: ctx.request.body.age,
   };
-
   const user = await Users.create(newUser);
   ctx.status = 201;
   ctx.body = user;
 };
 
 const deleteUser = async (ctx, next) => {
-  const id = ctx.params.id;
-  await Users.destroy({ where: { id } });
-  ctx.status = 204;
-  ctx.body = {};
+  const name = ctx.params.name;
+  await Users.destroy({ where: { name } }).then((response) => {
+    if (response == 0) {
+      ctx.throw(404, "User not found");
+      ctx.body = {};
+    } else {
+      ctx.status = 204;
+      ctx.body = {};
+    }
+  });
 };
 
 const updateUser = async (ctx, next) => {
-  const id = ctx.params.id;
+  const name = ctx.params.name;
   const { ...body } = ctx.request.body;
-  const user = await Users.update(body, { where: { id } });
-  ctx.status = 200;
-  ctx.body = user;
+  const user = await Users.update(body, { where: { name } });
+  if (!user) {
+    ctx.throw(404, "User not found");
+    ctx.body = {};
+  } else {
+    ctx.status = 200;
+    ctx.body = user;
+  }
 };
 
 module.exports = {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,17 @@
-const Router = require('koa-router');
-const userCRUD = require('../controllers/userController')
+const Router = require("koa-router");
+const userCRUD = require("../controllers/userController");
 
 const router = new Router();
 
-router.get('/', async (ctx) => {
-    // ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
-    ctx.body = `Seu servidor esta rodando!`; //http://localhost:3000/
+router.get("/", async (ctx) => {
+  // ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
+  ctx.body = `Seu servidor esta rodando!`; //http://localhost:3000/
 });
 
 router.get("/users", userCRUD.getAllUsers);
 router.get("/users/:name", userCRUD.getUser);
-// router.get("/users/:name", userCRUD.getUserByName);
 router.post("/users", userCRUD.createUser);
-router.put("/users/:id", userCRUD.updateUser);
-router.delete("/users/:id", userCRUD.deleteUser);
+router.put("/users/:name", userCRUD.updateUser);
+router.delete("/users/:name", userCRUD.deleteUser);
 
 module.exports = router;
